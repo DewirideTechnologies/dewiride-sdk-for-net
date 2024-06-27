12ec6511e8cd23facef2d264ecd6a29f7ff51134
Jagdish Kumawat <jagdish.kumawat@dewiride.com>
2024-06-27 15:54:58 +0530

    initial commit

---
+++ azure/storage/Azure.Storage.Table.Helper/Azure.Storage.Table.Helper/ITableStorageHelper.cs
@@ -0,0 +1,74 @@
+﻿using Azure.Data.Tables;
+
+namespace Azure.Storage.Table.Helper
+{
+    /// <summary>
+    /// Defines methods for interacting with Azure Table Storage.
+    /// </summary>
+    public interface ITableStorageHelper
+    {
+        /// <summary>
+        /// Asynchronously retrieves a single entity from Azure Table Storage.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="partitionKey">The partition key of the entity.</param>
+        /// <param name="rowKey">The row key of the entity.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the retrieved entity or null if not found.</returns>
+        Task<T?> GetEntityAsync<T>(string tableName, string partitionKey, string rowKey) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously retrieves an entity from Azure Table Storage by email.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="partitionKey">The partition key of the entity.</param>
+        /// <param name="email">The email of the entity.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the retrieved entity or null if not found.</returns>
+        Task<T?> GetEntityByEmailAsync<T>(string tableName, string partitionKey, string email) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously retrieves all entities from a table in Azure Table Storage that match the specified partition key.
+        /// </summary>
+        /// <typeparam name="T">The type of the entities.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="partitionKey">The partition key to filter by.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of retrieved entities.</returns>
+        Task<List<T>> GetEntitiesAsync<T>(string? tableName, string? partitionKey) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously retrieves the latest entity from a table in Azure Table Storage.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the retrieved entity or null if not found.</returns>
+        Task<T?> GetEntityAsync<T>(string? tableName) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously retrieves all entities from a table in Azure Table Storage that match the specified filter.
+        /// </summary>
+        /// <typeparam name="T">The type of the entities.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="filter">The filter to apply.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of retrieved entities.</returns>
+        Task<List<T>> GetEntitiesAsync<T>(string? tableName, FormattableString filter) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously inserts or merges an entity in Azure Table Storage.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="entity">The entity to upsert.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the Azure response.</returns>
+        Task<Response?> UpsertEntityAsync<T>(string? tableName, T entity) where T : class, ITableEntity, new();
+
+        /// <summary>
+        /// Asynchronously deletes a single entity from Azure Table Storage.
+        /// </summary>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="partitionKey">The partition key of the entity.</param>
+        /// <param name="rowKey">The row key of the entity.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the Azure response.</returns>
+        Task<Response> DeleteEntityAsync(string? tableName, string partitionKey, string rowKey);
+    }
+}
---
+++ azure/storage/Azure.Storage.Table.Helper/Azure.Storage.Table.Helper/TableStorageHelper.cs
@@ -0,0 +1,214 @@
+﻿using Azure.Data.Tables;
+using Microsoft.Extensions.Logging;
+
+namespace Azure.Storage.Table.Helper
+{
+    public class TableStorageHelper : ITableStorageHelper
+    {
+        private readonly ILogger<TableStorageHelper> _logger;
+        private readonly TableServiceClient _tableServiceClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TableStorageHelper"/> class.
+        /// </summary>
+        /// <param name="logger">The logger to use for logging information.</param>
+        /// <param name="tableServiceClient">The table service client for interacting with Azure Table Storage.</param>
+        public TableStorageHelper(ILogger<TableStorageHelper> logger, TableServiceClient tableServiceClient)
+        {
+            _logger = logger;
+            _tableServiceClient = tableServiceClient;
+        }
+
+        /// <summary>
+        /// Get Table Client Async and create table if not exists
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        private async Task<TableClient> GetTableClientAsync(string tableName)
+        {
+            var tableClient = _tableServiceClient.GetTableClient(tableName);
+            await tableClient.CreateIfNotExistsAsync();
+            return tableClient;
+        }
+
+        /// <summary>
+        /// Get a single entity from Azure Table Storage
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <param name="partitionKey"></param>
+        /// <param name="rowKey"></param>
+        /// <returns></returns>
+        public async Task<T?> GetEntityAsync<T>(string tableName, string partitionKey, string rowKey) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                var tableClient = await GetTableClientAsync(tableName);
+                return await tableClient.GetEntityAsync<T>(partitionKey: partitionKey, rowKey: rowKey);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Get Entity by Partition Key and Row Key --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously retrieves an entity from Azure Table Storage by email.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity to retrieve, must implement ITableEntity.</typeparam>
+        /// <param name="tableName">The name of the table to query.</param>
+        /// <param name="partitionKey">The partition key of the entity.</param>
+        /// <param name="email">The email address to query for.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the retrieved entity or null if not found.</returns>
+        /// <exception cref="Exception">Thrown when the table name is null or empty.</exception>
+        public async Task<T?> GetEntityByEmailAsync<T>(string tableName, string partitionKey, string email) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new Exception("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+
+                if (!string.IsNullOrEmpty(partitionKey) && !string.IsNullOrEmpty(email))
+                    return tableClient.Query<T>(filter: TableClient.CreateQueryFilter($"Email eq {email.ToLower()} and PartitionKey eq {partitionKey}")).FirstOrDefault();
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Get Entity by Email --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Gets all records from Azure Table Storage for the table name passed along with tenantId as the partition key
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <param name="partitionKey"></param>
+        /// <returns></returns>
+        public async Task<List<T>> GetEntitiesAsync<T>(string? tableName, string? partitionKey) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new Exception("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+
+                if (!string.IsNullOrEmpty(partitionKey))
+                    return tableClient.Query<T>(filter: TableClient.CreateQueryFilter($"PartitionKey eq {partitionKey}")).ToList();
+                else
+                    return tableClient.Query<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Get Entities by Partition Key --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Gets the latest record from Azure Table Storage for the table name passed
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public async Task<T?> GetEntityAsync<T>(string? tableName) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new NullReferenceException("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+
+                // return first item from ascending order on timestamp
+                return tableClient.Query<T>().OrderBy(x => x.Timestamp).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Get Entity --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Gets all records from Azure Table Storage for the table name passed along with the filter passed
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<List<T>> GetEntitiesAsync<T>(string? tableName, FormattableString filter) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new Exception("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+                var allData = tableClient.Query<T>(filter: TableClient.CreateQueryFilter(filter)).ToList();
+
+                return allData;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Get Entities by Filter --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Insert or Merge an entity into Azure Table Storage. If the entity exists, it will be replaced, else a new entity will be created.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task<Response?> UpsertEntityAsync<T>(string? tableName, T entity) where T : class, ITableEntity, new()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new Exception("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+                return await tableClient.UpsertEntityAsync(entity: entity, mode: TableUpdateMode.Replace);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Upsert Entity --> {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Delete a single entity from Azure Table Storage by tenantId as the partition key and aadObjectId as the row key
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="partitionKey"></param>
+        /// <param name="rowKey"></param>
+        /// <returns></returns>
+        public async Task<Response> DeleteEntityAsync(string? tableName, string partitionKey, string rowKey)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(tableName))
+                    throw new Exception("Table name cannot be null or empty");
+
+                var tableClient = await GetTableClientAsync(tableName);
+                return await tableClient.DeleteEntityAsync(partitionKey: partitionKey, rowKey: rowKey);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: Table Storage Helper Delete Entity --> {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
